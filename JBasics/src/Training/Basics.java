30415e042e0708409eb04f8f77b14d333a7e4f2d
luan <luan.d@curro.co.za>
2022-02-17 15:16:49 +0200

    15 Feb up to date

---
+++ JBasics/src/Training/Basics.java
@@ -0,0 +1,59 @@
+package Training;
+
+public class Basics {
+
+	public static void main(String[] args) {
+	
+//		// String stored and printed.
+//		String nameVariable = "John";
+//		System.out.println(nameVariable);
+//		
+//		// Integer stored and printed.
+//		int myNum = 15;
+//		System.out.println(myNum);
+//		
+//	    // Character stored and printed
+//		char charNum = 'N';
+//		System.out.println(charNum);
+//		
+//		//boolean value stored and printed.
+//		boolean happy = true;
+//		System.out.println(happy);
+//		
+//		// float value stored and printed.
+//		float decimal = 15.45f;
+//		System.out.println(decimal);
+//		
+//		int x = 160 + 40;
+//		System.out.println(x);
+//		
+//		int sum1 = x * 5;
+//		int sum2 = sum1/5 -10;
+//		System.out.println(sum2);
+//		
+//		int increment = 5;
+//		increment++;
+//		System.out.println(increment);
+//		
+//		int decrement = 10;
+//		decrement--;
+//		System.out.println(decrement);
+//		
+//		int modus = 5%2;
+//		System.out.println(modus);
+		
+//		int num1 = 10;
+//		int num2 = 5;
+//		System.out.println(!(num2 == 5));
+		
+		String text = "Vrkvbrkvber";
+		System.out.println(text.length());
+		System.out.println(text.toLowerCase());
+		System.out.println(text.toUpperCase());
+		
+		String index = "Please locate where 8 is in the char";
+		System.out.println(index.indexOf("8"));
+
+	}
+
+}
